d41cacb7a2961fc07390d7e459106163ab115ed1
BonBon <rgusiakov@gmail.com>
2022-06-15 11:05:50 +0300

    feat: mobile layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens : {
+      tablet : '860px',
+      desktop : '1200px'
+    },
+    colors : {
+      white : '#F6F6F6',
+      red : '#EE2B47',
+      black : '#2C2E3E',
+      'dark-grey' : '#34374C'
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addVariant }) => {
+      addVariant('hocus-active', ['&:focus', '&:hover', '&:active'])
+      addVariant('italic-child', '& > i')
+    })
+  ],
 }
